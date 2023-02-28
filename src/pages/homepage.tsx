a37085f47f93abceb5001c88c29b73b08f8622f3
PatiSacramento <patriciargsacramento@outlook.com>
2023-02-28 11:29:06 -0300

    [fix] homepage import

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -4,14 +4,13 @@ import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Kitnets } from '@/components/Kitnets'
 import { Sobre } from '@/components/Sobre'
-import OffcanvasExample from '@/components/Navbar'
+
 
 
 export default function HomePage() {
   return (
     <>
       <Header />
-     {/*  <OffcanvasExample /> */}
       <Banner />
       <Kitnets />
       <Regiao />
