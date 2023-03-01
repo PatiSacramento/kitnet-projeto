5974d226a7f51f94ec1b3e7d232685cf10377e30
PatiSacramento <patriciargsacramento@outlook.com>
2023-03-01 12:12:16 -0300

    [fix] maps

--- src/components/Regiao/index.tsx
+++ src/components/Regiao/index.tsx
@@ -20,18 +20,18 @@ export const Regiao = () => {
             <hr />
 
             <span>
-                <RegiaoCard name="Restaurante Gosto Sim" imagem={gostoSim} linkMapa="https://goo.gl/maps/AgqTz5Jpbkq3itcu6"/>
-                <RegiaoCard name="Restaurante Quintal Butantã" imagem={quintalButanta} linkMapa="https://goo.gl/maps/q2fW5KTYUYV9Bt1JA"/>
-                <RegiaoCard name="Churrascaria Tordilho" imagem={churrascariaTordilho} linkMapa="https://goo.gl/maps/f8anEj3Vfs71g64X9"/>
-                <RegiaoCard name="InkaHouse Butantã" imagem={inkaHouse} linkMapa="https://goo.gl/maps/7djBJH3mXtqo1bUn6"/>
-                <RegiaoCard name="Três Pescadores Butantã" imagem={tresPescadores} linkMapa="https://goo.gl/maps/ZfccDxsPJzwZ1NUH9"/>
-                <RegiaoCard name="Padaria Cidade Universitária" imagem={padariaCidadeUniversitaria} linkMapa="https://goo.gl/maps/t8khbzCb8Z4ZWcHa6"/>
-                <RegiaoCard name="Ultrafarma Vila Indiana" imagem={ultrafarma} linkMapa="https://goo.gl/maps/QcN1hkk9XBMpsb1c8"/>
-                <RegiaoCard name="Supermercado Violeta" imagem={supermercadoVioleta} linkMapa="https://goo.gl/maps/2w3Rh4obiBpHN42x7"/>
-                <RegiaoCard name="Villa Verde Pizzas" imagem={villaVerdePizzas} linkMapa="https://goo.gl/maps/9eFTm1eqE7GWWJMr6"/>
-                <RegiaoCard name="Kadalora Pizzaria" imagem={kadaloraPizzaria} linkMapa="https://goo.gl/maps/dDqFuRZdUdAx9WA8A"/>
-                <RegiaoCard name="Frutaria Jardim Rizzo" imagem={frutariaJardimRizzo} linkMapa="https://goo.gl/maps/7Jn5ND85J7Js4qn77"/>
-                <RegiaoCard name="Academia Smart Fit Corifeu" imagem={academiaSmartFitCorifeu} linkMapa="https://goo.gl/maps/pH81DptJyZfTcE4fA"/>
+                <RegiaoCard name="Restaurante Gosto Sim" imagem={gostoSim} linkMapa="https://goo.gl/maps/JvB15cUMh9EGGa267"/>
+                <RegiaoCard name="Restaurante Quintal Butantã" imagem={quintalButanta} linkMapa="https://goo.gl/maps/2xSvs9cd6wCiLfqn6"/>
+                <RegiaoCard name="Churrascaria Tordilho" imagem={churrascariaTordilho} linkMapa="https://goo.gl/maps/4AJfnTrePWtMA5Mj7"/>
+                <RegiaoCard name="InkaHouse Butantã" imagem={inkaHouse} linkMapa="https://goo.gl/maps/qTr9h4noxwn2kV4J6"/>
+                <RegiaoCard name="Três Pescadores Butantã" imagem={tresPescadores} linkMapa="https://goo.gl/maps/cetVR8Y7YBSqrjRt8"/>
+                <RegiaoCard name="Padaria Cidade Universitária" imagem={padariaCidadeUniversitaria} linkMapa="https://goo.gl/maps/AgEwFGAb9y8n5K8z6"/>
+                <RegiaoCard name="Ultrafarma Vila Indiana" imagem={ultrafarma} linkMapa="https://goo.gl/maps/SHTXQXd2sbNgo2Ki6"/>
+                <RegiaoCard name="Supermercado Violeta" imagem={supermercadoVioleta} linkMapa="https://goo.gl/maps/hXMr9jusiJFgcYZYA"/>
+                <RegiaoCard name="Villa Verde Pizzas" imagem={villaVerdePizzas} linkMapa="https://goo.gl/maps/tYryRxx2Xwetenr28"/>
+                <RegiaoCard name="Kadalora Pizzaria" imagem={kadaloraPizzaria} linkMapa="https://goo.gl/maps/W2tFxz2wu2XnyT6E7"/>
+                <RegiaoCard name="Frutaria Jardim Rizzo" imagem={frutariaJardimRizzo} linkMapa="https://goo.gl/maps/Jb3X3u5Gr3n3cypk8"/>
+                <RegiaoCard name="Academia Smart Fit Corifeu" imagem={academiaSmartFitCorifeu} linkMapa="https://goo.gl/maps/PUHpBiEQYgDXBMU66"/>
             </span>
         </RegiaoContainer>
     )
