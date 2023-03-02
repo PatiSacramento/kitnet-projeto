04dd94ee11ea3679b94d1083bd4d964e619d8ea0
PatiSacramento <patriciargsacramento@outlook.com>
2023-03-02 14:27:39 -0300

    [fix] media query problem

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,51 @@
 import Link from "next/link"
-import MediaQuery from "react-responsive"
 import { HeaderContainer, LinksMenu } from "./style"
 import { List } from "phosphor-react"
 import { Navbar } from "../Navbar"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 export const Header = () => {
     const [openSideMenu, setOpenSideMenu] = useState(false)
+    const [windowWidth, setWindowWidth] = useState(0)
 
+    useEffect(() => {
+        window.addEventListener("resize", (event) => {
+           setWindowWidth(window.screen.width) 
+        })
+        window.removeEventListener("resize", (event) => {
+            setWindowWidth(window.screen.width)
+        })
+        
+    }, [])
+   
+    
+   function windowSize(windowWidth: number) {
+        if(windowWidth <= 768){
+            return  <List size={30} onClick={openMenu}/> 
+        } else {
+            if (openSideMenu) {
+                return <Navbar isOpen={openSideMenu} openMenu={setOpenSideMenu}/> 
+            } else {
+                return (
+                    <>
+                        <LinksMenu href="/#kitnets">NOSSAS KITNETS</LinksMenu>
+                        <LinksMenu href="/#regiao">A REGIÃO</LinksMenu>
+                        <LinksMenu href="/#sobre">SOBRE NÓS</LinksMenu>
+                                
+                        <LinksMenu href="https://api.whatsapp.com/send?phone=5531999769422&text=Olá!%20Tenho%20interesse%20nas%20kitnets." target="_blank" rel="noreferrer">CONTATO</LinksMenu>
+                    </>  
+                )
+            }
+        }
+   }
+
+  
     function openMenu() {
         setOpenSideMenu(!openSideMenu)
     }
+
+
 
     return (
         <HeaderContainer> 
@@ -21,25 +55,8 @@ export const Header = () => {
                 </Link>
             </h1>
             <span>
-                <MediaQuery maxWidth={768}>
-                  <List size={30} onClick={openMenu}/>  
-                </MediaQuery>
-                  {openSideMenu ? 
-                    <Navbar isOpen={openSideMenu} openMenu={setOpenSideMenu}/> 
-
-                    : 
-
-                    <MediaQuery minWidth={769}>
-                        <LinksMenu href="/#kitnets">NOSSAS KITNETS</LinksMenu>
-                        <LinksMenu href="/#regiao">A REGIÃO</LinksMenu>
-                        <LinksMenu href="/#sobre">SOBRE NÓS</LinksMenu>
-                                
-                        <LinksMenu href="https://api.whatsapp.com/send?phone=5531999769422&text=Olá!%20Tenho%20interesse%20nas%20kitnets." target="_blank" rel="noreferrer">CONTATO</LinksMenu>
-                                
-                    </MediaQuery>
-                  }
-                  
-                  </span>
+                {windowSize(windowWidth)}                  
+            </span>
 
         </HeaderContainer >
     )
