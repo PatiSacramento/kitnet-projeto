a4bfdb0c50572030db557dd2157f66cfd2c2543b
PatiSacramento <patriciargsacramento@outlook.com>
2023-03-09 10:15:09 -0300

    [add] meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ export default class MyApp extends App {
         <Head>
           <title>KITUSP - Kitnets no Butantã</title>
           <meta name="description" content="Kitnets mobiliadas e moradias estudantis na região do Butantã, próxima à USP." />
+          <meta name='keywords' content='kitnet, kitnets, kitnets mobiliadas, kitnet mobiliada, moradia estudantil, moraridas estudantis, USP, Butantã, kitnet Butantã, kitnets butantã, kitnet usp, kitnets usp, moradia usp, moradia butantã' />
           <meta name="robots" content='index, follow' />
           <link rel="canonical" href="https://kitusp.com.br" />
 
