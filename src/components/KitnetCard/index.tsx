8f64687b4baa075327466c7c5e391ad0631618db
PatiSacramento <patriciargsacramento@outlook.com>
2023-03-09 10:51:46 -0300

    [fix] path

--- src/components/KitnetCard/index.tsx
+++ src/components/KitnetCard/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const KitnetCard = (props: { imgPrincipal: any, id: string, name: string } ) => {
     return (
-        <Link href={`/kitnet${props.id}`} as={props.id} >
+        <Link href={`/kitnet${props.id}`} as={`kitnet/butanta/${props.id}`} >
             <KitnetContainer className="grow">
                 <Image src={props.imgPrincipal} alt="" quality={100}/>
                 <p>{props.name.toUpperCase()}</p>
