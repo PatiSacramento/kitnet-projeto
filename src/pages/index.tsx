9b65b0b9358d364f54de952d96b86471d54f4732
PatiSacramento <patriciargsacramento@outlook.com>
2023-03-09 10:43:51 -0300

    [add] head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GlobalStyle } from '@/styles/global'
 import HomePage from './homepage'
 import styled from 'styled-components'
-
+import Head from 'next/head' 
 
 const PageContainer = styled.div`
   width: 100vw;
@@ -11,9 +11,18 @@ const PageContainer = styled.div`
 export default function Home() {
 
   return (
-    <PageContainer>
-      <GlobalStyle />
-      <HomePage />
-    </PageContainer>
+    <div>
+      <Head>
+        <title>KITUSP - Kitnets no Butantã</title>
+        <meta name="description" content="Kitnets mobiliadas e moradias estudantis na região do Butantã, próxima à USP." />
+        <meta name='keywords' content='kitnet, kitnets, kitnets mobiliadas, kitnet mobiliada, moradia estudantil, moraridas estudantis, USP, Butantã, kitnet Butantã, kitnets butantã, kitnet usp, kitnets usp, moradia usp, moradia butantã' />
+      </Head>
+
+      <PageContainer>
+
+        <GlobalStyle />
+        <HomePage />
+      </PageContainer>
+    </div>
   )
 }
