e4c2e36ad5a1ff86b53be08906249ade2ed80eb4
Iago Soriano <iago.soriano@truebuiltsoftware.com>
2025-02-21 13:06:10 -0300

    chore: reorder things and ad eslint plugin

--- src/app/kitnets/[kitName]/layout.tsx
+++ src/app/kitnets/[kitName]/layout.tsx
@@ -1,6 +1,11 @@
 import Carousel from '@/components/ui/Carousel';
 import { getImagesFromFolder } from '@/lib/getImagesFromFolder';
 import { ReactNode } from 'react';
+import Kit1 from './content/abadia-dos-dourados-1.mdx';
+import Kit2 from './content/abadia-dos-dourados-2.mdx';
+import Kit3 from './content/abadia-dos-dourados-3.mdx';
+import Kit4 from './content/loft-rua-corinto.mdx';
+import KitnetNavigationButton from '@/components/kitnet-navigation-button';
 
 export const metadata = {
   title: 'Kitnets do Elio',
@@ -11,12 +16,6 @@ type Params = {
   children: ReactNode;
 };
 
-import Kit1 from './content/abadia-dos-dourados-1.mdx';
-import Kit2 from './content/abadia-dos-dourados-2.mdx';
-import Kit3 from './content/abadia-dos-dourados-3.mdx';
-import Kit4 from './content/loft-rua-corinto.mdx';
-import KitnetNavigationButton from '@/components/kitnet-navigation-button';
-
 const contentMap: Record<string, { content: (args: any) => JSX.Element; name: string }> = {
   ['abadia-dos-dourados-1']: { content: Kit1, name: 'Kitnet Abadia dos Dourados 1' },
   ['abadia-dos-dourados-2']: { content: Kit2, name: 'Kitnet Abadia dos Dourados 2' },
@@ -26,22 +25,20 @@ const contentMap: Record<string, { content: (args: any) => JSX.Element; name: st
 
 const routeNames = Object.keys(contentMap);
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
   return routeNames.map((routeName) => ({ routeName }));
 }
 
-export default async function Page({ params }: Params) {
-  const { kitName: kitKey } = params;
-
+export default function Page({ params }: Params) {
   const images = [
-    ...getImagesFromFolder(`images/imagens-kitnets/${kitKey}`),
+    ...getImagesFromFolder(`images/imagens-kitnets/${params.kitName}`),
     ...getImagesFromFolder(`images/imagens-kitnets/area-comum`),
   ];
-  const ContentComponent = contentMap[kitKey].content;
+  const ContentComponent = contentMap[params.kitName].content;
 
   return (
     <>
-      <KitnetNavigationButton kitKey={kitKey} allKits={contentMap} />
+      <KitnetNavigationButton kitKey={params.kitName} allKits={contentMap} />
 
       <div className="flex flex-col w-[95%] mx-auto lg:w-[85%] items-center justify-between gap-8 px-4 py-12 md:flex-row md:px-16">
         <div className="text-left self-start ml-6 lg:ml-10 mt-6 lg:mt-20 md:w-1/2 md:text-left ml-10">
